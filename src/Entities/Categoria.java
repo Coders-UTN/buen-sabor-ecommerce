41a38feb2b2217f44141e41a170f0507e7e02a5a
rodrymza <rodrymza@gmail.com>
2025-05-27 02:07:41 -0300

    Inicializar coleccion de subcategorias en el constructor. Añadidos metodos para agregar y quitar subcategorias

--- src/Entities/Categoria.java
+++ src/Entities/Categoria.java
@@ -1,16 +1,17 @@
 package Entities;
+import java.util.HashSet;
 import java.util.Set;
 
 public class Categoria {
     private String denominacion;
     private Set<Categoria> subcategorias;
 
-
     public Categoria() {
     }
 
     public Categoria(String denominacion) {
         this.denominacion = denominacion;
+        this.subcategorias = new HashSet<>();
     }
 
     public String getDenominacion() {
@@ -27,5 +28,17 @@ public class Categoria {
 
     public void setSubcategorias(Set<Categoria> subcategorias) {
         this.subcategorias = subcategorias;
+    }
+
+    public void agregarSubcategoria(Categoria subcategoria){
+        this.subcategorias.add(subcategoria);
+    }
+
+    public void quitarSubcategoria(Categoria subcategoria){
+        if (this.subcategorias.remove(subcategoria)) {
+            System.out.println("Subcategoria " + subcategoria.getDenominacion() + " eliminada");
+        } else {
+            System.out.println("La subcategoria no existe en la categoria padre");
+        }
     }
 }

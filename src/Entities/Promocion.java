0f6c2c5cbfc2f1d6ffa6eb796a60c598483ddb02
rodrymza <rodrymza@gmail.com>
2025-05-27 02:09:57 -0300

    Correcciones de constructor, getters y setters. Nuevos metodos para agregar o quitar articulos

--- src/Entities/Promocion.java
+++ src/Entities/Promocion.java
@@ -14,7 +14,7 @@ public class Promocion extends Base{
     private LocalDate fechaHasta;
     private LocalTime horaDesde;
     private LocalTime horaHasta;
-    private String descripcion;
+    private String descripcionDescuento;
     private double precioPromocional;
     private TipoPromocion tipoPromocion;
     private Set<Imagen> imagenes;
@@ -23,17 +23,12 @@ public class Promocion extends Base{
     public Promocion() {
     }
 
-    public Promocion(TipoPromocion tipoPromocion, double precioPromocional, String descripcion, LocalTime horaHasta, LocalTime horaDesde, LocalDate fechaHasta, LocalDate fechaDesde, String denominacion) {
-        this.tipoPromocion = tipoPromocion;
-        this.precioPromocional = precioPromocional;
-        this.descripcion = descripcion;
-        this.horaHasta = horaHasta;
-        this.horaDesde = horaDesde;
-        this.fechaHasta = fechaHasta;
-        this.fechaDesde = fechaDesde;
+    public Promocion(String denominacion, String descripcion, TipoPromocion tipoPromocion) {
         this.denominacion = denominacion;
-        this.imagenes = new HashSet<>();
+        this.descripcionDescuento = descripcion;
+        this.tipoPromocion = tipoPromocion;
         this.articulosEnPromocion = new HashSet<>();
+        this.imagenes = new HashSet<>();
     }
 
     public String getDenominacion() {
@@ -76,12 +71,10 @@ public class Promocion extends Base{
         this.horaHasta = horaHasta;
     }
 
-    public String getDescripcion() {
-        return descripcion;
-    }
+    public String getDescripcion() { return descripcionDescuento; }
 
     public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
+        this.descripcionDescuento = descripcion;
     }
 
     public double getPrecioPromocional() {
@@ -114,5 +107,42 @@ public class Promocion extends Base{
 
     public void setArticulosEnPromocion(Set<Articulo> articulosEnPromocion) {
         this.articulosEnPromocion = articulosEnPromocion;
+    }
+    public void agregarArticuloPromocion(Articulo articulo){
+        this.articulosEnPromocion.add(articulo);
+        articulo.agregarPromocion(this);
+    }
+
+    public void eliminarArticuloPromocion(Articulo articulo){
+        if (this.articulosEnPromocion.remove(articulo)){
+            System.out.println("Articulo " + articulo.getDenominacion() + " eliminado de la promocion");
+        } else {
+            System.out.println("El articulo no existe en la lista de articulos en promocion");
+        }
+    }
+
+    public void agregarImagen(Imagen imagen){
+        this.imagenes.add(imagen);
+    }
+
+    public void eliminarImagen(Imagen imagen){
+        if (this.imagenes.remove(imagen)){
+            System.out.println("Imagen eliminada");
+        } else {
+            System.out.println("La imagen no se encuentra en la lista de imágenes");
+        }
+    }
+
+    public boolean estaVigente(){
+        LocalDate fechaHoy = LocalDate.now();
+        LocalTime horaHoy = LocalTime.now();
+
+        boolean esValidaPorFecha = (fechaHoy.isEqual(fechaDesde) || fechaHoy.isAfter(fechaDesde)) &&
+                (fechaHoy.isEqual(fechaHasta) || fechaHoy.isBefore(fechaHasta));
+
+        boolean esValidaPorHora = (horaHoy.equals(horaDesde) || horaHoy.isAfter(horaDesde)) &&
+                (horaHoy.equals(fechaHasta) || horaHoy.isBefore(horaHasta));
+
+        return esValidaPorFecha && esValidaPorHora;
     }
 }

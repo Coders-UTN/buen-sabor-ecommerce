96a5d26ccc21755cd776618782d4c0f3bb0f2950
rodrymza <rodrymza@gmail.com>
2025-05-27 02:12:03 -0300

    Cambios en constructor y metodos para calculos y muestras por consola

--- src/Entities/Pedido.java
+++ src/Entities/Pedido.java
@@ -6,8 +6,11 @@ import Entities.Enums.TipoEnvio;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Pedido extends Base {
+    private Cliente cliente;
     private LocalTime horaEstimadaFinalizacion;
     private double total;
     private double totalCosto;
@@ -15,6 +18,22 @@ public class Pedido extends Base {
     private TipoEnvio tipoEnvio;
     private FormaPago formaPago;
     private LocalDate fechaPedido;
+    private Set<DetallePedido> detallesDelPedido;
+    private static double PRECIO_BASE_ENVIO = 900;
+    private static double COSTO_MERCADO_PAGO = 0.05;
+
+    public Pedido(Cliente cliente,LocalDate fechaPedido, FormaPago formaPago, TipoEnvio tipoEnvio, Estado estado, LocalTime horaEstimadaFinalizacion) {
+        this.cliente = cliente;
+        this.fechaPedido = fechaPedido;
+        this.formaPago = formaPago;
+        this.tipoEnvio = tipoEnvio;
+        this.estado = estado;
+        this.horaEstimadaFinalizacion = horaEstimadaFinalizacion;
+        this.detallesDelPedido = new HashSet<>();
+    }
+
+    public Pedido() {
+    }
 
     public LocalTime getHoraEstimadaFinalizacion() {
         return horaEstimadaFinalizacion;
@@ -28,16 +47,8 @@ public class Pedido extends Base {
         return total;
     }
 
-    public void setTotal(double total) {
-        this.total = total;
-    }
-
     public double getTotalCosto() {
         return totalCosto;
-    }
-
-    public void setTotalCosto(double totalCosto) {
-        this.totalCosto = totalCosto;
     }
 
     public Estado getEstado() {
@@ -70,5 +81,56 @@ public class Pedido extends Base {
 
     public void setFechaPedido(LocalDate fechaPedido) {
         this.fechaPedido = fechaPedido;
+    }
+
+    public void agregarDetallePedido(DetallePedido nuevoDetalle) {
+        this.detallesDelPedido.add(nuevoDetalle);
+    }
+
+    public void calcularTotal(){
+        double total = 0.0;
+        for (DetallePedido detalle : detallesDelPedido) {
+            total += detalle.calcularSubtotal();
+        }
+        total += calcularCostoEnvio() + calcularComisionMP();
+        this.total = total;
+    }
+
+    public void calcularTotalCosto(){
+        double total = 0.0;
+        for (DetallePedido detalle : detallesDelPedido){
+            total += detalle.calcularCosto();
+        }
+        this.totalCosto = total;
+    }
+
+    public double calcularCostoEnvio(){
+        if (this.tipoEnvio == TipoEnvio.DELIVERY) {
+            return  PRECIO_BASE_ENVIO;
+        }
+        return 0.0;
+    }
+
+    public double calcularComisionMP(){
+        if (this.formaPago == FormaPago.MERCADOPAGO) {
+            return this.total * COSTO_MERCADO_PAGO;
+        }
+        return 0.0;
+    }
+
+    public void mostrarPedido(){
+        System.out.println("-------------------------------------------------");
+        System.out.println("Pedido Cliente: " + this.cliente.getApellido() + " " + this.cliente.getNombre() + "\n" +
+                "Fecha " + this.fechaPedido.toString() + "\n" +
+                "Medio de pago: " + this.formaPago.name() + "\n" +
+                "Detalle:" );
+        for (DetallePedido detalle : this.detallesDelPedido) {
+            System.out.println(detalle.getDetalle());
+        }
+        System.out.println("Envio          $" + calcularCostoEnvio());
+        System.out.println("Comision MP    $" + calcularComisionMP() +
+                "\nTotal costo    $" + this.totalCosto +
+                "\nTotal          $" + this.total);
+        System.out.println("-------------------------------------------------");
     }
 }
